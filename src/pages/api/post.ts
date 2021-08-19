37594cc18960037468ca299390384213b804d288
Zen <kefengong@gmail.com>
2021-08-19 21:26:42 +0800

    feat: ignore prefix & add date sort

--- src/pages/api/post.ts
+++ src/pages/api/post.ts
@@ -3,11 +3,12 @@ import glob from "glob";
 import fs from "fs/promises";
 import matter from "gray-matter";
 import { serialize } from "next-mdx-remote/serialize";
+import dayjs from "dayjs";
 
 const POST_DIR = path.join(process.cwd(), "_posts");
 
 export async function getPosts() {
-  const filePaths = glob.sync(`${POST_DIR}/**/*.mdx`);
+  const filePaths = glob.sync(`${POST_DIR}/**/[!_]*.{md,mdx}`);
   console.log(filePaths);
   return Promise.all(
     filePaths.map((path) => {
@@ -16,7 +17,11 @@ export async function getPosts() {
         .then((rawContent) => {
           const { content, data } = matter(rawContent);
           return {
-            data,
+            data: {
+              ...data,
+              date: dayjs(data.date).format("YYYY-MM-DD"),
+              tag: data.tags ?? [],
+            },
             content,
           };
         })
@@ -34,7 +39,12 @@ export async function getPosts() {
           });
         });
     })
-  );
+  ).then((list) => {
+    list.sort((a, z) => {
+      return dayjs(z.data.date).unix() - dayjs(a.data.date).unix();
+    });
+    return list;
+  });
 }
 
 function tap(v: any) {
